897d4dcfec47a971b950e85925d5acdc1d894a1c
DMC <PRIMARY.dmartinez@navantia.es>
2018-04-17 09:59:04 +0200

    Works, and when the popup is closed deletes the temporal file

--- index.js
+++ index.js
@@ -3,8 +3,10 @@
  MIT License.
 */
 
-var join  = require('path').join,
-    spawn = require('child_process').spawn;
+const fs    = require('fs'),
+      os    = require('os'),
+      path  = require('path'),
+      spawn = require('child_process').spawn;
 
 var Dialog = module.exports = {
 
@@ -33,11 +35,11 @@ var Dialog = module.exports = {
         os_name = process.platform,
         title   = title ? title : 'Important';
 
-    var str = (str + '').replace(/([.?*+^$[\]\\(){}<>|`-])/g, "\$1");
+    var str = (str + '').replace(/([.?*+^$[\]\\(){}<>|`-])/g, '\$1');
 
     // return codes for zenity are 0 on "OK" 
     // and 1 on "No/Cancel" or close window
-    if (os_name == 'linux') {
+    if (os_name === 'linux') {
 
       str = str.replace(/[<>]/g, '');
       cmd.push('zenity');
@@ -45,10 +47,11 @@ var Dialog = module.exports = {
       cmd.push('--text') && cmd.push(str);
       cmd.push('--title') && cmd.push(title);
       if (str.length > 30) cmd.push('--width') && cmd.push('300');
-
+	  this.run(cmd, callback);
+	  
     // return codes in macOS are exactly as in Linux
     // 0 for 'OK', 1 for 'Cancel'
-    } else if (os_name == 'darwin') {
+    } else if (os_name === 'darwin') {
       
       switch (type) { // Set dialog icon
         case 'error':
@@ -70,8 +73,9 @@ var Dialog = module.exports = {
       script += '\"' + str + '\" with title \"' + title + '\" buttons \"OK\"';
       script += ' with icon ' + type;
       cmd.push(script);
-
-    } else { // windows
+      this.run(cmd, callback);
+	  
+    } else if (os_name === 'win32') { // windows
 
       // return codes for Windows (default value minus 1)
       // defaults in https://www.tutorialspoint.com/vbscript/vbscript_dialog_boxes.htm
@@ -101,14 +105,63 @@ var Dialog = module.exports = {
       str = str.replace(/"/g, "'"); // double quotes to single quotes
      
       // msgbox.vbs script from http://stackoverflow.com/questions/774175
-      cmd.push('cscript');
-      cmd.push(join(__dirname, 'msgbox.vbs'));
-      cmd.push(str) && cmd.push(type) && cmd.push(title);
-
-    }
-
-    this.run(cmd, callback);
-
+	  
+	  let vbScriptPath = path.join(__dirname, 'msgbox.vbs');
+	  // Detect packaged applications that save the script in a snapshot that cannot be accessed from cscript
+	  console.log(`Script in: ${vbScriptPath}`);
+	  fs.stat(vbScriptPath, (err, stats) => {
+		console.log(`Script located: ${err || stats}`);
+	    if (err) {
+		  callback && callback(err && err.code, '', err && err.message);
+		  return;
+	    }
+		cmd.push('cscript');
+		cmd.push('//NOLOGO');
+		cmd.push(vbScriptPath);
+		cmd.push(str) && cmd.push(type) && cmd.push(title);
+		console.log(`Composed cmd: ${cmd.join(' ')}`);
+		if (stats.ino !== 0) {
+          this.run(cmd, callback);
+		} else {
+			console.log(`Not a real inode`);
+			console.log(`Create ${os.tmpdir()}/${require.module}`);
+		  fs.mkdtemp(path.join(os.tmpdir(), 'dialog-'), (err, folder) => {
+			let vbDestination = path.join(folder, 'msgbox.vbs');
+			let callbackClean = (code, stdout, stderr) => {
+				fs.unlink(vbDestination, (err) => {
+					console.log(`rm: ${err}`);
+					fs.rmdir(folder, (err) => {
+						console.log(`rmdir: ${err}`);
+						callback && callback(code, stdout, stderr);
+					});
+				});
+			};
+			console.log(`Temp dir created: ${err || folder}`);
+			if (err) {
+			  callbackClean(err && err.code, '', err && err.message);
+			  return;
+			}
+			fs.readFile(vbScriptPath, (err, data) => {
+				  if (err) {
+					callbackClean(err && err.code, '', err && err.message);
+					return;
+				  }
+				fs.writeFile(vbDestination, data, (err) => {
+				  console.log(`Script copied: ${err || vbDestination}`);	
+				  if (err) {
+					callbackClean(err && err.code, '', err && err.message);
+					return;
+				  }
+				  cmd[2] = vbDestination;
+				  this.run(cmd, callbackClean);
+				});
+			});
+		  });
+		}
+	  });
+    } else {
+	  callback && callback(-1, '', 'Unsupported platform');
+	}
   },
 
   run: function(cmd, cb) {
@@ -117,9 +170,7 @@ var Dialog = module.exports = {
         stdout = '',
         stderr = '';
 
-    var child = spawn(bin, args, {
-      detached: true
-    });
+    var child = spawn(bin, args);
 
     child.stdout.on('data', function(data) {
       stdout += data.toString();
